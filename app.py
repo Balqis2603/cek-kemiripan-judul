6aeb9285f50f50f614e13f28f58791dfd46d4262
Balqis2603 <balqiss283@gmail.com>
2026-02-12 00:57:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ if uploaded_file is not None:
             })
 
             hasil = hasil.sort_values(
-                by=["Skor Kemiripan (%)", "Tahun"],
+                by=["Skor Kemiripan", "Tahun"],
                 ascending=[False, True]
             ).reset_index(drop=True)
 
@@ -48,4 +48,5 @@ if uploaded_file is not None:
 
         else:
             st.warning("Masukkan judul terlebih dahulu.")
+
 
